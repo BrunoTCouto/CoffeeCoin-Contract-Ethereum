114ad867cd75f3c20d8720be8471b2fc53c8131c
Bruno Tomassetti Couto <brutom@br.ibm.com>
2018-04-21 17:27:45 -0300

    function buyCoffee
    function buyCoffee

--- contracts/CoffeeCoin.sol
+++ contracts/CoffeeCoin.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.17;
 
 contract CoffeeCoin{
 
-
+    // CONSTRUCTING MY "OBJECTS" =======================
     struct CoffeeMachine{
         uint priceByUnit;
         uint stock;
@@ -12,14 +12,18 @@ contract CoffeeCoin{
     struct User{
         uint credit;
     }
+    // CONSTRUCTING MY "OBJECTS" =======================
     
+    // DECLARING SOMETHING
     mapping (address=>CoffeeMachine) coffeeMachines;
     mapping (address=>User) users;
 
+    // CONSTRUCTOR
     function CoffeeCoin() public{
         users[msg.sender] = User(500);
     }
 
+    // COFFEE MACHINE FUNCTIONS =======================
     function newCoffeeMachine(address coffeeMachineAddr, uint initialPriceByUnit, uint initialStock) public{
         coffeeMachines[coffeeMachineAddr] = CoffeeMachine(initialPriceByUnit, initialStock, 0);
     }
@@ -28,7 +32,26 @@ contract CoffeeCoin{
         coffeeMachines[coffeeMachineAddr].stock += quantity; 
     }
 
+    function changeCoffeePrice(address coffeeMachineAddr, uint newPriceByUnit) public{
+        coffeeMachines[coffeeMachineAddr].priceByUnit = newPriceByUnit; 
+    }
+
     function auditCoffeeMachine(address coffeeMachineAddr) public constant returns(address, uint ,uint, uint){
         return (coffeeMachineAddr, coffeeMachines[coffeeMachineAddr].priceByUnit, coffeeMachines[coffeeMachineAddr].stock, coffeeMachines[coffeeMachineAddr].credit);
     }
+    // COFFEE MACHINE FUNCTIONS =======================
+
+    // USER FUNCTIONS =======================
+    function buyCoffee(address CoffeeMachineAddress) public returns(bool){ //NOT DONE
+        if (users[msg.sender].credit > coffeeMachines[CoffeeMachineAddress].priceByUnit && coffeeMachines[CoffeeMachineAddress].stock >= 1) {
+            coffeeMachines[CoffeeMachineAddress].stock -= 1;
+            coffeeMachines[CoffeeMachineAddress].credit += coffeeMachines[CoffeeMachineAddress].priceByUnit;
+            users[msg.sender].credit -= coffeeMachines[CoffeeMachineAddress].priceByUnit;
+            return true;
+        }
+        return false;
+    }
+
+    // USER FUNCTIONS =======================
+
 }

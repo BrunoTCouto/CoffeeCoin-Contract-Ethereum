960d74444366c01aa840d120e58a619bfec577be
Bruno Tomassetti Couto <brutom@br.ibm.com>
2018-04-23 00:23:04 -0300

    2 new functions and events added
    2 new functions and events added

--- contracts/CoffeeCoin.sol
+++ contracts/CoffeeCoin.sol
@@ -36,21 +36,38 @@ contract CoffeeCoin{
     // COFFEE MACHINE FUNCTIONS =======================
     function newCoffeeMachine(address coffeeMachineAddr, int initialPriceByUnit, int initialStock) public onlySeller{
         coffeeMachines[coffeeMachineAddr] = CoffeeMachine(initialPriceByUnit, initialStock, 0);
+        emit newCoffeMachineInNetwork(coffeeMachineAddr);
     }
+    event newCoffeMachineInNetwork(address coffeeMachine);
 
     function addStockToCoffeeMachine(address coffeeMachineAddr, int quantity) public onlySeller{
         require(quantity > 0);
         coffeeMachines[coffeeMachineAddr].capsules += quantity; 
+        emit machineReStocked(coffeeMachineAddr, coffeeMachines[coffeeMachineAddr].capsules);
     }
+    event machineReStocked(address coffeeMachine, int newStock);
 
     function changeCoffeePrice(address coffeeMachineAddr, int newPriceByUnit) public onlySeller{
         require(newPriceByUnit > 0);
         coffeeMachines[coffeeMachineAddr].priceByUnit = newPriceByUnit; 
+        emit newCoffeePrice(coffeeMachineAddr, newPriceByUnit);
     }
+    event newCoffeePrice(address coffeeMachine, int newPrice);
 
     function auditCoffeeMachine(address coffeeMachineAddr) public onlySeller constant returns(address, int ,int, int){
         return (coffeeMachineAddr, coffeeMachines[coffeeMachineAddr].priceByUnit, coffeeMachines[coffeeMachineAddr].capsules, coffeeMachines[coffeeMachineAddr].credit);
     }
+
+    function coffeeMachineStatus(address coffeeMachineAddr) public constant returns(address, int ,int){
+        return (coffeeMachineAddr, coffeeMachines[coffeeMachineAddr].priceByUnit, coffeeMachines[coffeeMachineAddr].capsules);
+    }
+
+    function colectCoffeeMachineCredit(address coffeeMachineAddr) public onlySeller{
+        users[msg.sender].credit += coffeeMachines[coffeeMachineAddr].credit;
+        coffeeMachines[coffeeMachineAddr].credit -= coffeeMachines[coffeeMachineAddr].credit;
+        emit creditsColected(msg.sender, coffeeMachineAddr);
+    }
+    event creditsColected(address colector, address coffeeMachine);
     // COFFEE MACHINE FUNCTIONS =======================
 
     // USER FUNCTIONS =======================
@@ -59,16 +76,20 @@ contract CoffeeCoin{
         coffeeMachines[CoffeeMachineAddress].capsules -= 1;
         coffeeMachines[CoffeeMachineAddress].credit += coffeeMachines[CoffeeMachineAddress].priceByUnit;
         users[msg.sender].credit -= coffeeMachines[CoffeeMachineAddress].priceByUnit;
+        emit coffeeSold(msg.sender, CoffeeMachineAddress);
     }
+    event coffeeSold(address to, address coffeeMachine);
 
     function msgSenderCreditsBalance() public constant returns(int){
         return users[msg.sender].credit;
     }
 
-    function addCredits(address userAddr, int quantity) public onlySeller{
+    function addCredits(address userAddr, int quantity) public onlySeller{ //NOT WORKING AS EXPECTED
         require(quantity > 0);
         users[userAddr].credit += quantity;
+        emit creditsAdded(userAddr, quantity);
     }
+    event creditsAdded(address userAddr, int quantity);
     // USER FUNCTIONS =======================
 
 }
